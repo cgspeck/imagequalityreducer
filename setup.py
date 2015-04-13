c37c7f92df7bc2d8886ae2652b52bd6d356c6eaa
Chris Speck <cgspeck@gmail.com>
2015-04-13 22:22:56 +1000

    Setup and requirements file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open('requirements-test.txt') as f:
+    required_test = f.read().splitlines()
+
+setup(
+    name='Image Quality Reducer',
+    description='''
+    Reduce images to desired file size
+    ''',
+    version='0.9.0',
+    author='Chris Speck',
+    author_email='cgspeck@gmail.com',
+    url='http://www.chrisspeck.com/',
+    packages=['qr'],
+    entry_points={
+        'console_scripts': [
+            'qr = qualityreducer.utilrunner:main'
+        ]
+    },
+    install_requires=required,
+    extras_require={
+    'tests': required_test
+    }
+)
